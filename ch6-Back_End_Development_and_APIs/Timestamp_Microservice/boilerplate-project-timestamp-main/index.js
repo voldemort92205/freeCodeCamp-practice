59d6ff904434d5ceab95fd514bb991f5f5d72203
perry92205 <voldemort92205@gmail.com>
2023-09-23 23:00:44 +0800

    complete project: Timestamp Microservice - v1

--- ch6-Back_End_Development_and_APIs/Timestamp_Microservice/boilerplate-project-timestamp-main/index.js
+++ ch6-Back_End_Development_and_APIs/Timestamp_Microservice/boilerplate-project-timestamp-main/index.js
@@ -8,25 +8,47 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
+/*
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
+*/
 
+app.get("/api/:date?", function(req, res) {
+  let date = undefined;
+  if (req.params.date === undefined) {
+    date = new Date();
+  } else {
+    date = new Date(req.params.date)
+  }
+  if (/^\d*$/.test(req.params.date)) {
+    // handle epoch time...
+    date.setTime(req.params.date)
+  }
+  let output = {};
+  if (date.toGMTString() === "Invalid Date") {
+    output["error"] = "Invalid Date";
+  } else {
+    output["unix"] = date.getTime();
+    output["utc"] = date.toGMTString();
+  }
+  res.json(output)
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
