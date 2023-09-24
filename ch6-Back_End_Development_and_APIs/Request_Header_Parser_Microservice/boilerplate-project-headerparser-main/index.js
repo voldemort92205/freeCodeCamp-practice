89c6df53e2571bb973fde36f6888cbf4bf32be1d
perry92205 <voldemort92205@gmail.com>
2023-09-24 10:22:53 +0800

    complete project: Request Header Parser Microservice - v1

--- ch6-Back_End_Development_and_APIs/Request_Header_Parser_Microservice/boilerplate-project-headerparser-main/index.js
+++ ch6-Back_End_Development_and_APIs/Request_Header_Parser_Microservice/boilerplate-project-headerparser-main/index.js
@@ -14,17 +14,27 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+app.set('trust proxy', true);
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function(req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get("Accept-Language"),
+    software: req.get("user-agent")
+  })
+})
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
