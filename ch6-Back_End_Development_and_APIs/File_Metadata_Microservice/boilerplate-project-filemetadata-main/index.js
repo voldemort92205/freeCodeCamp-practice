d7300831282b2afecb1bca4b958ca716efdba3ed
perry92205 <voldemort92205@gmail.com>
2023-09-30 14:50:15 +0800

    complete the project: File Metadata Microservice - v1

--- ch6-Back_End_Development_and_APIs/File_Metadata_Microservice/boilerplate-project-filemetadata-main/index.js
+++ ch6-Back_End_Development_and_APIs/File_Metadata_Microservice/boilerplate-project-filemetadata-main/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require("multer");
+
+const upload = multer({ storage: multer.memoryStorage() })
+
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +11,21 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post("/api/fileanalyse", upload.single('upfile'), function(req, res) {
+  //console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
